95bd89b9500ba3fc0214d5a9b66b536c31461b10
ingridslette <ingrid.slette@gmail.com>
2025-04-15 11:53:56 -0500

    updated to get daily rather than monthly precip

--- extract-mswep-ppt.R
+++ extract-mswep-ppt.R
@@ -1,36 +1,42 @@
 # Ingrid Slette
-# started 13 September 2024
+# started February 19, 2025
 
-# getting MSWEP (http://www.gloh2o.org/mswep/) precipitation data for NutNet sites
-# request access to MSWEP data, which is shared via Google Drive
-# install and set up rclone to acess MSWEP Google Drive
+# retrieving MSWEP (http://www.gloh2o.org/mswep/) precipitation data for lter and ltar sites
+# for LTER working group Resiliance and Managment
 
-# I copied all years of monthly MSWEP data into a file on my Desktop using the R terminal and the following code
-# MacBook-Pro-169:~ ingridslette$ rclone  copy --drive-shared-with-me gdrive:MSWEP_V280/Past/Monthly /Users/ingridslette/Desktop/mswep
-# MacBook-Pro-169:~ ingridslette$ rclone  copy --drive-shared-with-me gdrive:MSWEP_V280/NRT/Monthly /Users/ingridslette/Desktop/mswep
+# first have to request access to MSWEP data, which is shared via Google Drive
+# then install and set up rclone to access MSWEP Google Drive
+
+# I copied daily MSWEP precip data from all years into a file on my Desktop (so as to not fill up the shared wg google drive) 
+# by running the following code in the R terminal: 
+# rclone  copy --drive-shared-with-me gdrive:MSWEP_V280/Past/Daily /Users/ingridslette/Desktop/mswep_daily
+# rclone  copy --drive-shared-with-me gdrive:MSWEP_V280/NRT/Daily /Users/ingridslette/Desktop/mswep_daily
 
 library(terra)
 library(lubridate)
 library(tidyverse)
 
-# read in list of all NutNet site codes and coordinates 
-sites <- read.csv("/Users/ingridslette/Desktop/NutNet/NutNet-site-coords.csv")
+# read in list of sites and coordinates 
+sites <- read.csv("/Users/ingridslette/Desktop/NutNet/site_coords.csv")
+
+unique(sites$site_code)
 
 # make that a SpatVector
 site <- sites %>% vect(geom = c("longitude", "latitude"), crs = "EPSG:4326")
 
 # list all of the monthly mswep precip data files
-r_paths <- list.files("/Users/ingridslette/Desktop/mswep",
+# change this to location to which you downloaded these files
+r_paths <- list.files("/Users/ingridslette/Desktop/mswep_daily",
                       full.names = TRUE) %>% 
   sort()
 
 # make that a SpatRaster
 r <- rast(r_paths)
 
-# extract monthly precip data for each site
-ppt_monthly <- terra::extract(r, site, bind = TRUE)
+# extract daily precip data for each site
+ppt_daily <- terra::extract(r, site, bind = TRUE)
 
-df <- as.data.frame(ppt_monthly)
+df <- as.data.frame(ppt_daily)
 
 names(df) <- c("site_code", paste0("precip_", time(r)))
 
@@ -38,17 +44,14 @@ out <- pivot_longer(df, -"site_code", names_to = "date",
                     values_to = "precip") %>% 
   mutate(date = str_replace(date, "^precip_", ""))
 
-out$date <- substr(out$date, 1, 7)
-
-# Create a new column for the year
+# create a new column for the year
 out$year <- substr(out$date, 1, 4)
 
-# Create a new column for the month
+# create a new column for the month
 out$month <- substr(out$date, 6, 7)
 
-# filter out 1979 and 2024 - don't have data for all months
-out <- filter(out, year != 1979)
-out <- filter(out, year != 2024)
+# create a new column for the day
+out$day <- substr(out$date, 9, 10)
 
-write.csv(out, file = "/Users/ingridslette/Dropbox/NutNet data/climate/mswep/mswep-monthly-ppt-all-nutnet-sites.csv")
+write.csv(out, file = '/Users/ingridslette/Library/CloudStorage/GoogleDrive-slett152@umn.edu/Shared drives/NutNet_DRAGNet_Shared/NutNet Shared/NutNet Non-Core Data/weather/MSWEP/precip-daily-mswep2.csv')
 
