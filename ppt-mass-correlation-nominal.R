e4965d2132c8093a2b06098cb8f69a2662f6c08f
ingridslette <ingrid.slette@gmail.com>
2025-10-05 13:00:28 -0500

    trying different threshold for extreme ppt years, deleted graphing code i wasn't using

--- ppt-mass-correlation-nominal.R
+++ ppt-mass-correlation-nominal.R
@@ -4,11 +4,10 @@
 
 # Filter to keep only ppt values within non-extreme range
 mass_ppt_nominal <- mass_ppt %>%
-  filter(ppt > p10_ppt, ppt < p90_ppt)
+  filter(ppt > p05_ppt, ppt < p95_ppt)
 
 View(mass_ppt_nominal)
 unique(mass_ppt_nominal$site_code)
-
 
 ### Main model
 
@@ -22,7 +21,6 @@ hist(resid, breaks = 30, main = "Histogram of Residuals")
 qqnorm(resid)
 qqline(resid)
 plot(fitted(main_model_nominal), resid, main = "Residuals vs Fitted")
-
 
 ### Models per site
 
@@ -215,7 +213,7 @@ ggplot(data = mass_ppt_nominal, aes(x = ppt, y = live_mass, color = trt, shape =
   scale_color_manual(values = c("#0092E0", "#ff924c")) +
   theme_bw(14)
 
-ggplot(mass_ppt, aes(x = ppt, y = live_mass, color = trt)) +
+ggplot(mass_ppt_nominal, aes(x = ppt, y = live_mass, color = trt)) +
   geom_point() +
   geom_line(data = predictions, aes(x = 10^log_ppt, y = predicted_mass), linewidth = 1) +
   labs(x = "Growing Season Precipitation (mm)", y = "Biomass (g/m2)", color = "Treatment") +
@@ -224,90 +222,13 @@ ggplot(mass_ppt, aes(x = ppt, y = live_mass, color = trt)) +
   scale_color_manual(values = c("#0092E0", "#ff924c")) +
   theme(legend.position = "bottom")
 
-ggplot(mass_ppt, aes(x = ppt, y = live_mass, color = site_code)) +
+ggplot(mass_ppt_nominal, aes(x = ppt, y = live_mass, color = site_code)) +
   geom_line(data = predictions, aes(x = 10^log_ppt, y = predicted_mass), linewidth = 1) +
   geom_line(data = predictions_allsites, aes(x = 10^log_ppt, y = predicted_mass), 
             linewidth = 1, color = "black") +
   labs(x = "Growing Season Precipitation (mm)", y = "Biomass (g/m2)") +
   facet_wrap(~ trt) +
   theme_bw(14)
-
-predictions <- predictions %>%
-  left_join(results_with_averages, by = c("site_code", "trt"))
-
-fig2_control <- ggplot(subset(predictions, trt == "Control"), aes(x = 10^log_ppt, y = predicted_mass)) +
-  geom_line(aes(group = site_code, colour = r2)) +
-  geom_ribbon(data = subset(predictions_allsites, trt == "Control"),
-              aes(x = 10^log_ppt, ymin = mass_lower, ymax = mass_upper), 
-              fill = "#0092E0", alpha = 0.3) +
-  geom_line(data = subset(predictions_allsites, trt == "Control"), 
-            aes(x = 10^log_ppt, y = predicted_mass),
-            color = "#0092E0") +
-  geom_ribbon(data = subset(predictions_allsites, trt == "NPK"),
-              aes(x = 10^log_ppt, ymin = mass_lower, ymax = mass_upper), 
-              fill = "#ff924c", alpha = 0.15) +
-  geom_line(data = subset(predictions_allsites, trt == "NPK"), 
-            aes(x = 10^log_ppt, y = predicted_mass),
-            color = "#ff924c", linetype = "dashed") +
-  labs(x = "Growing Season Precipitation (mm)", y = "Biomass (g/m²)", 
-       title = "Control", colour = expression(R^2)) +
-  scale_y_continuous(limits = c(0, 2300)) +
-  scale_colour_gradient2(low = "#E4D3EE", mid = "#B185DB", high = "#423073",
-                         midpoint = 0.3) +
-  theme_bw() +
-  theme(
-    plot.title = element_text(hjust = 0.5, size = 14, face = "bold"),
-    axis.title = element_text(size = 14),
-    legend.title = element_text(size = 14),
-    legend.text = element_text(size = 10),
-    axis.title.y = element_text(size = 14, margin = margin(r = 20))
-  )
-
-fig2_npk <- ggplot(subset(predictions, trt == "NPK"), aes(x = 10^log_ppt, y = predicted_mass)) +
-  geom_line(aes(group = site_code, colour = r2)) +
-  geom_ribbon(data = subset(predictions_allsites, trt == "NPK"),
-              aes(x = 10^log_ppt, ymin = mass_lower, ymax = mass_upper), 
-              fill = "#ff924c", alpha = 0.3) +
-  geom_line(data = subset(predictions_allsites, trt == "NPK"), 
-            aes(x = 10^log_ppt, y = predicted_mass),
-            color = "#ff924c") +
-  geom_ribbon(data = subset(predictions_allsites, trt == "Control"),
-              aes(x = 10^log_ppt, ymin = mass_lower, ymax = mass_upper), 
-              fill = "#0092E0", alpha = 0.15) +
-  geom_line(data = subset(predictions_allsites, trt == "Control"), 
-            aes(x = 10^log_ppt, y = predicted_mass),
-            color = "#0092E0", linetype = "dashed") +
-  labs(x = "Growing Season Precipitation (mm)", y = "Biomass (g/m²)", 
-       title = "Fertilized", colour = expression(R^2)) +
-  scale_y_continuous(limits = c(0, 2300)) +
-  scale_colour_gradient2(low = "#E4D3EE", mid = "#B185DB", high = "#423073",
-                         midpoint = 0.3) +
-  theme_bw() +
-  theme(
-    plot.title = element_text(hjust = 0.5, size = 14, face = "bold"),
-    axis.title = element_text(size = 14)
-  )
-
-fig2_both <- ggarrange(
-  fig2_control + rremove("xlab"),
-  fig2_npk + 
-    rremove("ylab") + rremove("xlab") +
-    theme(
-      axis.text.y = element_blank(), 
-      axis.ticks.y = element_blank()
-    ),
-  ncol = 2,
-  common.legend = TRUE,
-  legend = "right",
-  align = 'hv'
-)
-
-fig2_both <- annotate_figure(
-  fig2_both,
-  bottom = text_grob("Growing Season Precipitation (mm)", size = 14)
-)
-
-fig2_both
 
 
 
